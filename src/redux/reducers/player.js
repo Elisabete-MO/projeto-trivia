832e23f004cefbb3e702138b07a753d354ee740e
Willian Dutra <contatowilliandutra@gmail.com>
2022-11-09 20:07:03 -0300

    Requisitos 13 e 14 - Inicio dos requisitos esperando os anteriores

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -2,8 +2,8 @@ import { PLAYER_LOGIN } from '../actions';
 
 const INITIAL_STATE = {
   name: '', // nome-da-pessoa,
-  assertions: '', // número-de-acertos,
-  score: '', // pontuação,
+  assertions: 0, // número-de-acertos,
+  score: 0, // pontuação,
   gravatarEmail: '', // email-da-pessoa,
 };
 
