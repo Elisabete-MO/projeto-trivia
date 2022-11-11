fcb83f318ee4972d7214da56ff8608ef93e05def
Scarlat Pereira <engcivil.scarlat@gmail.com>
2022-11-11 09:18:32 -0300

    Requisito 18 - Inicio do requisito esperando os anteriores

---
+++ src/pages/Ranking.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+
+class Ranking extends React.Component {
+  state = {
+    redirectLogin: false,
+  };
+
+  render() {
+    const { redirectLogin } = this.state;
+    if (redirectLogin) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div>
+        <h1 data-testid="ranking-title">Ranking Page</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => { this.setState({ redirectLogin: true }); } }
+        >
+          Play Again
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Ranking;
